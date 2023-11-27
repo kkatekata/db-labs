ab7c6a39fdf75ca1590493020971fd81c889db34
kkatekata <kata672004@gmail.com>
2023-11-27 16:16:21 +0400

    лр 4

--- db-labs/lr4/3.4.sql
+++ db-labs/lr4/3.4.sql
@@ -1,5 +1,7 @@
 /*Добавьте еще одно spa с такими же характеристиками как в задании 1, сгенерировав новое ID.*/
 USE cd;
-INSERT facilities
-SELECT (SELECT MAX(facid)+1 FROM facilities), facility, membercost, guestcost, initialoutlay, monthlymaintenance FROM facilities where facid = 9;
-SELECT * FROM facilities;
+INSERT INTO facilities(facid, facility, membercost, guestcost, initialoutlay, monthlymaintenance)
+SELECT 1 + (SELECT MAX(facid) FROM facilities), facility, membercost, guestcost, initialoutlay, monthlymaintenance
+FROM facilities
+WHERE facility LIKE 'Spa'
+LIMIT 1;
